bd972998e820f88ce4dc6801963e75ccb25bba9f
Danylo Ostrikov <danyloostrikov@gmail.com>
2019-03-14 00:05:15 +0200

    +added struct of game

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
+#include <conio.h>
 using namespace std;
+
+char Map[10][10];
+
+void getMapFromTxt();
+void printMap();
+void logic();
+bool isGameOver();
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    cout << "press any key to start the game\n";
+    getch();
+    while(isGameOver()){
+
+    }
 }
